905ae8344622a70d2f6aeaa1adeffaddc0eadf1a
artandrey <artandrey7777@gmail.com>
2025-09-18 22:23:04 +0300

    fix: rollup bundling mjs and cjs with d.ts content, imported files where not included to the resulting build

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,28 +1,31 @@
-import typescript from '@rollup/plugin-typescript';
+import nodeResolve from '@rollup/plugin-node-resolve';
 import del from 'rollup-plugin-delete';
 import { dts } from 'rollup-plugin-dts';
+import esbuild from 'rollup-plugin-esbuild';
 
-export default {
-  input: 'src/lib/index.ts',
-  output: [
-    {
-      file: 'dist/index.cjs',
-      format: 'cjs',
-    },
-    {
-      file: 'dist/index.mjs',
-      format: 'esm',
-    },
-    {
-      file: 'dist/index.d.ts',
-      format: 'es',
-    },
-  ],
-  plugins: [
-    del({ targets: 'dist/*' }),
-    typescript({
-      tsconfig: './tsconfig.prod.json',
-    }),
-    dts(),
-  ],
-};
+export default [
+  {
+    input: 'src/lib/index.ts',
+    output: [
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+      },
+      {
+        file: 'dist/index.mjs',
+        format: 'esm',
+      },
+    ],
+    plugins: [nodeResolve(), del({ targets: 'dist/*' }), esbuild()],
+  },
+  {
+    input: 'src/lib/index.ts',
+    output: [
+      {
+        file: 'dist/index.d.ts',
+        format: 'es',
+      },
+    ],
+    plugins: [dts()],
+  },
+];
